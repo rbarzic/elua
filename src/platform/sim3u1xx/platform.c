d49338c050563bac3ef6bda01e58c8520977dea5
James Snyder <jbsnyder@fanplastic.org>
2012-05-10 15:27:13 -0500

    don't enable vbus invalid interrupt in sleep

--- src/platform/sim3u1xx/platform.c
+++ src/platform/sim3u1xx/platform.c
@@ -984,7 +984,7 @@ void sim3_pmu_sleep( unsigned seconds )
   // Switch VREG to low power mode
   SI32_VREG_A_disable_band_gap( SI32_VREG_0 );
   SI32_VREG_A_enter_suspend_mode( SI32_VREG_0 );
-  SI32_VREG_A_enable_vbus_invalid_interrupt( SI32_VREG_0 );
+  //SI32_VREG_A_enable_vbus_invalid_interrupt( SI32_VREG_0 );
 
   // Disable VDD Monitor
   SI32_VMON_A_disable_vdd_supply_monitor(SI32_VMON_0);
@@ -1043,7 +1043,7 @@ void sim3_pmu_pm9( unsigned seconds )
   // Switch VREG to low power mode
   SI32_VREG_A_disable_band_gap( SI32_VREG_0 );
   SI32_VREG_A_enter_suspend_mode( SI32_VREG_0 );
-  SI32_VREG_A_enable_vbus_invalid_interrupt( SI32_VREG_0 );
+  //SI32_VREG_A_enable_vbus_invalid_interrupt( SI32_VREG_0 );
 
   // Disable VDD Monitor
   SI32_VMON_A_disable_vdd_supply_monitor(SI32_VMON_0);
